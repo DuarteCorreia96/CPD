edd080b4a3523e9cc865d28bafb675667330328e
Duarte Correia <duartecorreiascs@hotmail.com>
2019-05-14 10:46:18 +0100

    added tests

---
+++ cell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=Group10
+#SBATCH --output=cell_%j.out
+#SBATCH --error=cell_%j.err
+#SBATCH --ntasks=80
+#SBATCH --cpus-per-task=4
+srun ./simpar-ompi 2 10 100000000 5
+srun ./simpar-ompi 2 20 100000000 5
+srun ./simpar-ompi 2 30 100000000 5
+srun ./simpar-ompi 2 40 100000000 5
+srun ./simpar-ompi 2 50 100000000 5
+srun ./simpar-ompi 2 60 100000000 5
+srun ./simpar-ompi 2 70 100000000 5
+srun ./simpar-ompi 2 90 100000000 5
+srun ./simpar-ompi 2 100 100000000 5
+srun ./simpar-ompi 2 250 100000000 5
+srun ./simpar-ompi 2 500 100000000 5
+srun ./simpar-ompi 2 1000 100000000 5
+
---
+++ part.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=Group10
+#SBATCH --output=part_%j.out
+#SBATCH --error=part_%j.err
+#SBATCH --ntasks=80
+#SBATCH --cpus-per-task=4
+srun ./simpar-ompi 2 20 100 5
+srun ./simpar-ompi 2 20 1000 5
+srun ./simpar-ompi 2 20 10000 5
+srun ./simpar-ompi 2 20 100000 5
+srun ./simpar-ompi 2 20 1000000 5
+srun ./simpar-ompi 2 20 10000000 5
+srun ./simpar-ompi 2 20 100000000 5
+srun ./simpar-ompi 2 20 1000000000 5
+
